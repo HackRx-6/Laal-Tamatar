42e8cb9745787c7c4c784c9121fdf772cb41549a
arpan-lol <arpantaneja14@gmail.com>
2025-08-22 20:40:55 +0000

    Add solution for finding smallest starting index of almost equal substring to pattern in ROUND_6 folder

--- ROUND_6/find_almost_equal.py
+++ ROUND_6/find_almost_equal.py
@@ -1,22 +1,18 @@
 def find_almost_equal_index(s, pattern):
-    len_s = len(s)
-    len_p = len(pattern)
-    
-    for i in range(len_s - len_p + 1):
-        substring = s[i:i+len_p]
+    n, m = len(s), len(pattern)
+    for i in range(n - m + 1):
+        substring = s[i:i+m]
         diff_count = 0
-        for j in range(len_p):
+        for j in range(m):
             if substring[j] != pattern[j]:
                 diff_count += 1
-            if diff_count > 1:
-                break
+                if diff_count > 1:
+                    break
         if diff_count <= 1:
             return i
     return -1
 
-# Given values
 s = "ababbababa"
 pattern = "bacaba"
-
 result = find_almost_equal_index(s, pattern)
 print(result)
