0d11b05218c8348f7c034fbda04b2bfedca293e2
arpan-lol <arpantaneja14@gmail.com>
2025-08-22 19:19:47 +0000

    Add solution for finding smallest almost equal substring index in ROUND_6 folder

--- ROUND_6/almost_equal.py
+++ ROUND_6/almost_equal.py
@@ -1,21 +1,30 @@
-def find_almost_equal_index(s, pattern):
-    m, n = len(s), len(pattern)
-    if n > m:
-        return -1
-    
-    for i in range(m - n + 1):
-        substring = s[i:i+n]
-        diff_count = 0
-        for j in range(n):
-            if substring[j] != pattern[j]:
-                diff_count += 1
+# Given strings
+s = "ababbababa"
+pattern = "bacaba"
+
+# Function to check if two strings are almost equal
+# i.e., differ in at most one character
+
+def is_almost_equal(x, y):
+    diff_count = 0
+    for i in range(len(x)):
+        if x[i] != y[i]:
+            diff_count += 1
             if diff_count > 1:
-                break
-        if diff_count <= 1:
+                return False
+    return True
+
+# Function to find smallest start index of a substring in s almost equal to pattern
+
+def smallest_almost_equal_index(s, pattern):
+    n = len(s)
+    m = len(pattern)
+    for i in range(n - m + 1):
+        substring = s[i:i+m]
+        if is_almost_equal(substring, pattern):
             return i
     return -1
 
-s = "abcdefg"
-pattern = "bcdffg"
-result = find_almost_equal_index(s, pattern)
+# Call the function and print result
+result = smallest_almost_equal_index(s, pattern)
 print(result)
