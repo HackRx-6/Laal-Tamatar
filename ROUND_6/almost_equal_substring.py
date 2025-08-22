acd15b4443b0014ae4855e218d5420aa154ba4af
arpan-lol <arpantaneja14@gmail.com>
2025-08-22 19:21:06 +0000

    Add solution for finding smallest starting index of almost equal substring in ROUND_6 folder

--- ROUND_6/almost_equal_substring.py
+++ ROUND_6/almost_equal_substring.py
@@ -1,17 +1,21 @@
-def smallest_almost_equal_index(s, pattern):
+def almost_equal_substring_index(s, pattern):
     len_s = len(s)
     len_p = len(pattern)
     
     for i in range(len_s - len_p + 1):
         substring = s[i:i+len_p]
-        # Count the differences
-        diff_count = sum(1 for x, y in zip(substring, pattern) if x != y)
+        diff_count = 0
+        for a, b in zip(substring, pattern):
+            if a != b:
+                diff_count += 1
+                if diff_count > 1:
+                    break
         if diff_count <= 1:
             return i
     return -1
 
-s = "ababbababa"
-pattern = "bacaba"
+s = "abcdefg"
+pattern = "bcdffg"
 
-result = smallest_almost_equal_index(s, pattern)
-print(result)  # This will print the output to be captured
+result = almost_equal_substring_index(s, pattern)
+print(result)
