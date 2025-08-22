f6c9109e78ca1bbeb5299ca0a4585b0ad3e765fd
Deven-puri <devenpuri298@gmail.com>
2025-08-22 14:04:13 +0530

    Add llm

--- utils/llms.py
+++ utils/llms.py
@@ -1,3 +1,14 @@
-def get_llm(name: str):
-    # Returns the langchain llm object of the agent.
-    pass
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_llm(model: str):
+    llm = ChatOpenAI(
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        base_url=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        model=model
+    )
+    return llm
